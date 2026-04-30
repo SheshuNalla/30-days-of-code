95f576e50eb6bed36fdef140ca16e442cec63718
SheshuNalla <sheshunalla002@gmail.com>
2026-04-30 21:55:13 +0530

    comments

--- Day9.py
+++ Day9.py
@@ -1,7 +1,7 @@
 def fact(n):
     if n==0:
         return 1
-    return n * fact(n-1)
+    return n * fact(n-1) # Recursion is a programming technique where a function calls itself to solve a problem.
 
 
 ans = fact(4)
